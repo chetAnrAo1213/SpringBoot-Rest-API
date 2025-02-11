5815a5a131235bc494e46779ccc2571f916d2c7a
Chetan Rao <167339150+chetAnrAo1213@users.noreply.github.com>
2025-02-11 15:30:54 +0530

    Added Versioning under WebServices

---
+++ Web Services/Versioning/VersioningModel2.java
@@ -0,0 +1,26 @@
+package com.boot.Versioning;
+
+public class VersioningModel2 
+{
+	VersioningModel version2;
+
+	public VersioningModel getVersion2() {
+		return version2;
+	}
+
+	public void setVersion2(VersioningModel version2) {
+		this.version2 = version2;
+	}
+
+	public VersioningModel2() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public VersioningModel2(VersioningModel version2) {
+		super();
+		this.version2 = version2;
+	}
+	
+	
+}
