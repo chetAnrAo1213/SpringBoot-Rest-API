d71948d1761452ac4cb01bcb7f2709dd8addc139
Chetan Rao <167339150+chetAnrAo1213@users.noreply.github.com>
2025-02-11 22:55:12 +0530

    Delete Web Services/JsonJayway directory

--- Web Services/JsonJayway/JayWayAPI.java
+++
@@ -1,9 +0,0 @@
-package com.boot.JsonJayway;
-
-public class JayWayAPI 
-{
-
-	public static final String API_KEY = "pub_65377ed2d771886288d43cc24c5c7dbcc6b24";
-	public static String url = "https://newsdata.io/api/1/latest?apikey=pub_65377ed2d771886288d43cc24c5c7dbcc6b24&q=";
-	
-}
